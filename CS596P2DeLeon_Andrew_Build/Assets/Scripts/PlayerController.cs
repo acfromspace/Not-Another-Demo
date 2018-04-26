086d5748b88f57308bde1536e011bee989d3fb10
AC De Leon <andrewchristopherdeleon@gmail.com>
2018-04-26 14:13:40 -0700

    [+] Player and bullet prefabs, PlayerController.cs

---
+++ CS596P2DeLeon_Andrew_Build/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    void Update()
+    {
+        // Responsible for controlling respective player (so not 1 player can control everyone)
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        // Player movement
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        // Create bullet from prefab
+        GameObject bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        // Add velocity to bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6.0f;
+
+        // Destroy bullet
+        Destroy(bullet, 2);
+    }
+
+    // Displays own player as color
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.green;
+    }
+}
